feff2439fb1de007db16290ffbf7e7e405c6d869
godevp <73740267+godevp@users.noreply.github.com>
2022-10-02 15:36:55 -0400

    added comment for A1

--- Assets/[Scripts]/MainMenu_script.cs
+++ Assets/[Scripts]/MainMenu_script.cs
@@ -3,6 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+/*
+  MainMenu_scripts.cs 
+  Vitaliy Karabanov
+  Student number: 101312885
+  Last changed on Oct 2, 2022
+  This is the mainMenu and just a button scripts
+  Revision hystory: added function StartGame and QuitGame, later Surrender and restartB
+ */
+
+
 public class MainMenu_script : MonoBehaviour
 {
     // Start is called before the first frame update
